f917acaaae702208245f607e2d0007eca32370e3
nigechadameda <otakujanai@protonmail.com>
2024-09-22 19:04:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+console.log('Im being read');
+
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/loaders/GLTFLoader.js';
 import { EXRLoader } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/loaders/EXRLoader.js';
 
-console.log('Im being read');
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
