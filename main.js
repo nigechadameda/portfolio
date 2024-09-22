df8a8454c8be6ed08d91caaa61cf083e6908b41f
nigechadameda <otakujanai@protonmail.com>
2024-09-22 19:17:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 console.log('Im being read');
 
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/loaders/GLTFLoader.js';
-import { EXRLoader } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/loaders/EXRLoader.js';
 
