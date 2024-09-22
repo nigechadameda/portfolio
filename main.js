fa145de596c5fe46ff94f9356019f1930b1a27f8
nigechadameda <otakujanai@protonmail.com>
2024-09-22 19:20:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('Im being read');
+
+import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.154.0/examples/jsm/loaders/GLTFLoader.js';
+import { EXRLoader } from 'https://unpkg.com/three@0.154.0/examples/jsm/loaders/EXRLoader.js';
+
